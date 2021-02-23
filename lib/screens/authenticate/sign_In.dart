00b7be603ff118a234cda71a19a0ded0b2a4b9a8
Muaz Zulkarnain <idmuazzulkarnain@gmail.com>
2021-02-23 21:22:42 +0800

    Validator for Sign In

--- lib/screens/authenticate/sign_In.dart
+++ lib/screens/authenticate/sign_In.dart
@@ -11,10 +11,12 @@ class SignIn extends StatefulWidget {
 
 class _SignInState extends State<SignIn> {
   final AuthService _auth = AuthService();
+  final _formKey = GlobalKey<FormState>();
 
   //text field state
   String email = '';
   String password = '';
+  String error = '';
 
   @override
   Widget build(BuildContext context) {
@@ -37,10 +39,12 @@ class _SignInState extends State<SignIn> {
       body: Container(
         padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
         child: Form(
+          key: _formKey,
           child: Column(
             children: [
               SizedBox(height: 20.0),
               TextFormField(
+                validator: (val) => val.isEmpty ? 'Enter an email' : null,
                 onChanged: (val) {
                   setState(() => email = val);
                 },
@@ -48,6 +52,9 @@ class _SignInState extends State<SignIn> {
               SizedBox(height: 20.0),
               TextFormField(
                 obscureText: true,
+                validator: (val) => val.length < 6
+                    ? 'Password must be more than 6 characters'
+                    : null,
                 onChanged: (val) {
                   setState(() => password = val);
                 },
@@ -60,9 +67,18 @@ class _SignInState extends State<SignIn> {
                   style: TextStyle(color: Colors.white),
                 ),
                 onPressed: () async {
-                  print(email);
-                  print(password);
+                  if (_formKey.currentState.validate()) {
+                    print("valid");
+                    // if (result == null) {
+                    //   setState(() => error = 'Please Enter A Valid Email');
+                    // }
+                  }
                 },
+              ),
+              SizedBox(height: 12.0),
+              Text(
+                error,
+                style: TextStyle(color: Colors.red, fontSize: 14.0),
               ),
             ],
           ),
