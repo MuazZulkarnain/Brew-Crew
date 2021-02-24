d5c1ef04b0d64be290bd70f9b20b254e909ba374
Muaz Zulkarnain <idmuazzulkarnain@gmail.com>
2021-02-24 22:53:59 +0800

    Firestore Stream

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,7 +1,6 @@
 import 'package:brew_crew/home/home.dart';
 import 'package:brew_crew/models/user.dart';
 import 'package:brew_crew/screens/authenticate/authenticate.dart';
-import 'package:brew_crew/screens/authenticate/sign_In.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
