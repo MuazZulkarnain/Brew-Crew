c5b92cadb0fe123b45236a2064fb1c7980d237af
Muaz Zulkarnain <idmuazzulkarnain@gmail.com>
2021-02-24 21:41:58 +0800

    setup firestore cloud

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,4 +1,5 @@
 import 'package:brew_crew/models/user.dart';
+import 'package:brew_crew/services/database.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService {
@@ -47,6 +48,9 @@ class AuthService {
       AuthResult result = await _auth.createUserWithEmailAndPassword(
           email: email, password: password);
       FirebaseUser user = result.user;
+
+      //create new document for new user with the uid
+      await DatabaseService(uid: user.uid).updateUserData('0', 'Muaz Zul', 100);
       return _userFromFirebaseUser(user);
     } catch (e) {
       print(e.toString());
