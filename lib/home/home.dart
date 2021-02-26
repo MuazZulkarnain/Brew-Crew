7fb73399dddbdb99cec31829d83659de70c6c713
Muaz Zulkarnain <idmuazzulkarnain@gmail.com>
2021-02-26 23:14:16 +0800

    Update RaisedButton

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,3 +1,4 @@
+import 'package:brew_crew/home/settings_form.dart';
 import 'package:brew_crew/services/auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -16,7 +17,7 @@ class Home extends StatelessWidget {
           builder: (context) {
             return Container(
               padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 60.0),
-              child: Text('bottom sheet'),
+              child: SettingsForm(),
             );
           });
     }
